99c6801be862571c4a17e785808f01b25cec650d
Suraj Pathak <surajpathak2057@gmail.com>
2022-06-24 09:50:03 +0545

    Part 1: Rotating Cube

---
+++ main.cpp
@@ -0,0 +1,190 @@
+#include "olcConsoleGameEngine.h"
+
+// Represent coordinates in 3D space
+struct vec3d
+{
+	float x, y, z; 
+};
+
+// Group together 3 vec3d (vertices) to represent triangle
+struct triangle
+{
+	vec3d p[3];
+};
+
+// Group together triangles to represent object
+struct mesh
+{
+	std::vector<triangle> tris;
+};
+
+// 4x4 matrix
+struct mat4x4 {
+	float m[4][4] = { 0 };
+};
+
+
+class olcEngine3D : public olcConsoleGameEngine
+{
+private:
+	mesh meshCube;
+	mat4x4 matProj;
+
+	float fTheta;
+
+	void MultiplyMatrixVector(vec3d& i, vec3d& o, mat4x4& m) {
+		o.x = i.x * m.m[0][0] + i.y * m.m[1][0] + i.z * m.m[2][0] + m.m[3][0];
+		o.y = i.x * m.m[0][1] + i.y * m.m[1][1] + i.z * m.m[2][1] + m.m[3][1];
+		o.z = i.x * m.m[0][2] + i.y * m.m[1][2] + i.z * m.m[2][2] + m.m[3][2];
+		float w = i.x * m.m[0][3] + i.y * m.m[1][3] + i.z * m.m[2][3] + m.m[3][3];
+
+		if (w != 0.0f)
+		{
+			o.x /= w; o.y /= w; o.z /= w;
+		}
+	}
+
+public:
+	olcEngine3D()
+	{
+		m_sAppName = L"3D Demo"; // Name of application
+	}
+
+	bool OnUserCreate() override
+	{
+		// Populate mesh with vertecies data to define cube made of triangles
+		meshCube.tris = {
+
+			// SOUTH
+			{ 0.0f, 0.0f, 0.0f,    0.0f, 1.0f, 0.0f,    1.0f, 1.0f, 0.0f },
+			{ 0.0f, 0.0f, 0.0f,    1.0f, 1.0f, 0.0f,    1.0f, 0.0f, 0.0f },
+
+			// EAST                                                      
+			{ 1.0f, 0.0f, 0.0f,    1.0f, 1.0f, 0.0f,    1.0f, 1.0f, 1.0f },
+			{ 1.0f, 0.0f, 0.0f,    1.0f, 1.0f, 1.0f,    1.0f, 0.0f, 1.0f },
+
+			// NORTH                                                     
+			{ 1.0f, 0.0f, 1.0f,    1.0f, 1.0f, 1.0f,    0.0f, 1.0f, 1.0f },
+			{ 1.0f, 0.0f, 1.0f,    0.0f, 1.0f, 1.0f,    0.0f, 0.0f, 1.0f },
+
+			// WEST                                                      
+			{ 0.0f, 0.0f, 1.0f,    0.0f, 1.0f, 1.0f,    0.0f, 1.0f, 0.0f },
+			{ 0.0f, 0.0f, 1.0f,    0.0f, 1.0f, 0.0f,    0.0f, 0.0f, 0.0f },
+
+			// TOP                                                       
+			{ 0.0f, 1.0f, 0.0f,    0.0f, 1.0f, 1.0f,    1.0f, 1.0f, 1.0f },
+			{ 0.0f, 1.0f, 0.0f,    1.0f, 1.0f, 1.0f,    1.0f, 1.0f, 0.0f },
+
+			// BOTTOM                                                    
+			{ 1.0f, 0.0f, 1.0f,    0.0f, 0.0f, 1.0f,    0.0f, 0.0f, 0.0f },
+			{ 1.0f, 0.0f, 1.0f,    0.0f, 0.0f, 0.0f,    1.0f, 0.0f, 0.0f },
+
+		};
+
+		// Projection Matrix
+		float fNear = 0.1f;		// Near plane
+		float fFar = 1000.0f;	// Far plane
+		float fFov = 90.0f;		// Field of view (angle)
+		float fAspectRatio = (float)ScreenHeight() / (float)ScreenWidth();
+		float fFovRad = 1.0f / tanf(fFov * 0.5f / 180.0f * 3.14159f);	// Tangent calculation
+
+		matProj.m[0][0] = fAspectRatio * fFovRad;
+		matProj.m[1][1] = fFovRad;
+		matProj.m[2][2] = fFar / (fFar - fNear);
+		matProj.m[3][2] = (-fFar * fNear) / (fFar - fNear);
+		matProj.m[2][3] = 1.0f;
+		matProj.m[3][3] = 0.0f;
+
+		// Tell game engine everything is fine and continue running
+		return true;
+	}
+
+	bool OnUserUpdate(float fElapsedTime) override
+	{
+		// Clear the screen
+		Fill(0, 0, ScreenWidth(), ScreenHeight(), PIXEL_SOLID, FG_BLACK);
+
+		// Matrices to perform rotation transform around the axes
+		mat4x4 matRotZ, matRotX;
+		// To give impression that something is rotating, define angle value that changes over time
+		fTheta += 1.0f * fElapsedTime;
+
+		// Rotation Z
+		matRotZ.m[0][0] = cosf(fTheta);
+		matRotZ.m[0][1] = sinf(fTheta);
+		matRotZ.m[1][0] = -sinf(fTheta);
+		matRotZ.m[1][1] = cosf(fTheta);
+		matRotZ.m[2][2] = 1;
+		matRotZ.m[3][3] = 1;
+
+		// Rotation X
+		matRotX.m[0][0] = 1;
+		matRotX.m[1][1] = cosf(fTheta * 0.5f);
+		matRotX.m[1][2] = sinf(fTheta * 0.5f);
+		matRotX.m[2][1] = -sinf(fTheta * 0.5f);
+		matRotX.m[2][2] = cosf(fTheta * 0.5f);
+		matRotX.m[3][3] = 1;
+
+
+		// Draw Triangles
+		for (auto tri : meshCube.tris) 
+		{
+			triangle triProjected, triTranslated, triRotatedZ, triRotatedZX;
+
+			// Rotatae in Z-Axis
+			MultiplyMatrixVector(tri.p[0], triRotatedZ.p[0], matRotZ);
+			MultiplyMatrixVector(tri.p[1], triRotatedZ.p[1], matRotZ);
+			MultiplyMatrixVector(tri.p[2], triRotatedZ.p[2], matRotZ);
+
+			// Rotate in X-Axis
+			MultiplyMatrixVector(triRotatedZ.p[0], triRotatedZX.p[0], matRotX);
+			MultiplyMatrixVector(triRotatedZ.p[1], triRotatedZX.p[1], matRotX);
+			MultiplyMatrixVector(triRotatedZ.p[2], triRotatedZX.p[2], matRotX);
+
+			// Offset into the screen
+			// Translate triangle away from the camera
+			triTranslated = triRotatedZX;
+			triTranslated.p[0].z = triRotatedZX.p[0].z + 3.0f;
+			triTranslated.p[1].z = triRotatedZX.p[1].z + 3.0f;
+			triTranslated.p[2].z = triRotatedZX.p[2].z + 3.0f;
+
+			// Project triangles form 3D --> 2D
+			MultiplyMatrixVector(triTranslated.p[0], triProjected.p[0], matProj);
+			MultiplyMatrixVector(triTranslated.p[1], triProjected.p[1], matProj);
+			MultiplyMatrixVector(triTranslated.p[2], triProjected.p[2], matProj);
+
+			// Scale into view
+			// Projection matrix gives result triProjected betn -1 to +1 (normalized) so scale it to viewing area of the console screen
+			for (int i = 0; i <= 2; i++) {
+				// First change values between -1 to +1 to between 0 to +2 (positive)
+				triProjected.p[i].x += 1.0f;
+				triProjected.p[i].y += 1.0f;
+				// Divide by 2 and scale it to the appropriate size of the axis using screen width and height
+				triProjected.p[i].x *= 0.5f * (float)ScreenWidth();
+				triProjected.p[i].y *= 0.5f * (float)ScreenHeight();
+			}
+
+			// Rasterize Triangle
+			DrawTriangle(triProjected.p[0].x, triProjected.p[0].y,
+				triProjected.p[1].x, triProjected.p[1].y,
+				triProjected.p[2].x, triProjected.p[2].y,
+				PIXEL_SOLID, FG_WHITE);
+
+		}
+
+		return true;
+	}
+};
+
+int main() {
+	// Create object instance
+	olcEngine3D demo;
+
+	// Create instance of console of (256 character wide, 240 character height, each pixel of 4x4)
+	if (demo.ConstructConsole(200, 150, 4, 4))
+		demo.Start();
+	else
+		throw("Can't create console");
+
+	return 0;
+}
